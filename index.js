f3c81beea275eed17176ec34e14206815274b148
GYUYOUNG KANG <kgy@smartdisk.org>
2021-03-19 19:44:03 +0900

    0.0.15 android string key toLowerCase

--- index.js
+++ index.js
@@ -250,6 +250,7 @@ var AppresString = /** @class */ (function () {
 
             if(hash=="md5") id = md5(id);
             else if(hash=="sha1") id = sha1(id);
+            else id = td.toLowerCase();
 
             if(str.startsWith("<string-array>") && str.endsWith("</string-array>")) {
                 let s = str.substring(14, str.length-15);
